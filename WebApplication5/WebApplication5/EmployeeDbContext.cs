c5cecb35075a54e86dcefebfc9251f2e2372d57f
KhanBazeela <bazeelakhan9@gmail.com>
2024-07-11 15:58:09 +0530

    ist

---
+++ WebApplication5/WebApplication5/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication5.Entities;
+
+namespace WebApplication5
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WebApplication5/WebApplication5/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication5;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<EmployeeDbContext>(opts => opts.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Integrated Security=true; Initial Catalog=EmployeeDb"));
+builder.Services.AddDbContext<EmployeeDbContext>(opts => opts.UseSqlServer(builder.Configuration["ConnectionString"]));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
